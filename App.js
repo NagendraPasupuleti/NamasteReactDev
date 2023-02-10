e6d16d6e46319311363af25670e3d3b1a19648b7
Nagendra Pasupuleti <nagpasupuleti143@gmail.com>
2023-02-10 12:22:21 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+const heading = React.createElement(
+  "h3",
+  {
+    id: "title1",
+  },
+  "Heading 1"
+);
+const heading2 = React.createElement(
+  "h4",
+  {
+    id: "title2",
+  },
+  "Heading 2"
+);
+const container = React.createElement("div", { id: "container" }, [
+  heading,
+  heading2,
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
